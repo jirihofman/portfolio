a99a3ad13e985443c023da6fa09abc3286377799
Jiří Hofman <jiri.hofman@gmail.com>
2023-04-24 21:53:32 +0200

    fix: add pointer cursor for article

--- app/projects/article.jsx
+++ app/projects/article.jsx
@@ -8,7 +8,7 @@ export const Article = ({ project }) => {
 
 	return (
 		<Link href={appLink} title={`Click to view the ${project.homepage ? 'app' : 'repo'}.`} legacyBehavior>
-			<article className="p-4 md:p-8">
+			<article className="p-4 md:p-8 cursor-pointer">
 				<div className="flex justify-between gap-2 items-center">
 					<span className="text-xs duration-1000 text-zinc-200 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
 						{/* <Image src={`https://raw.githubusercontent.com/jirihofman/${project.name}/${project.default_branch}/public/favicon.ico`} alt={project.name} width={24} height={24} placeholder="blur" /> */}
