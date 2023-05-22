cbc8010fb04a3e757c6a999c1fb634216fe3762a
Jiří Hofman <jiri.hofman@gmail.com>
2023-05-22 22:09:00 +0200

    popover under menu

--- app/components/popover.jsx
+++ app/components/popover.jsx
@@ -23,7 +23,7 @@ const Popover = ({ button, content }) => {
 				{button}
 			</button>
 			{isHovered && (
-				<div className="absolute right-1 mt-2 w-48 bg-gray-300 bg-opacity-90 border border-gray-300 rounded-lg shadow-lg z-50">
+				<div className="absolute right-1 mt-2 w-48 bg-gray-300 bg-opacity-90 border border-gray-300 rounded-lg shadow-lg z-40">
 					<div className="p-2 text-sm text-black">
 						{content}
 					</div>
