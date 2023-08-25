b3f5ed27bb7162ff599046073f818f8d246e6a7e
Jiří Hofman <jiri.hofman@gmail.com>
2023-08-25 21:22:06 +0200

    refactor: recent activity, projects info - larger recent activity font, change wording - correct info on project page when viewing some else's projects

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -59,13 +59,13 @@ export default async function ProjectsPage({
     return (
         <div className="relative pb-16">
             <Navigation />
-            <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
+            <div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-12 md:pt-24 lg:pt-32">
                 <div className="max-w-2xl mx-auto lg:mx-0">
                     <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
                         Projects
                     </h2>
                     <p className="mt-4 text-zinc-400">
-                        {data.description}
+                        {customUsername ? `${customUsername}'s projects` : data.description}
                         {/* <pre>{JSON.stringify(vercelProjects.projects[1], null, 4)}</pre> */}
                     </p>
                 </div>
