6d8720d6126f0398a029e54d8569042607d612e2
Jiří Hofman <jiri.hofman@gmail.com>
2023-12-02 23:04:59 +0100

    feat: load GITHUB_USERNAME from GitHub API using GH_TOKEN

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,24 @@ const nextConfig = {
 	experimental: {
 		// mdxRs: true,
 	},
+	env: {
+		/** GitHub username loaded in build time. */
+		GITHUB_USERNAME: await fetch('https://api.github.com/user',
+			{
+				headers: {
+					Authorization: `token ${process.env.GH_TOKEN}`,
+				},
+				next: {
+					// No revalidation needed. It is fine to get it on build time and use it forever.
+					tags: ['github', 'github-username'],
+				}
+			}
+		).then(res => res.json()).then(data => data.login),
+	},
 	images: {
-		domains: [
-			"avatars.githubusercontent.com",
-			"github.com",
-			"raw.githubusercontent.com",
+		remotePatterns: [
+			{ protocol: 'https', hostname: '**.githubusercontent.com' },
+			{ protocol: 'https', hostname: '**.github.com' }
 		],
 	},
 };
