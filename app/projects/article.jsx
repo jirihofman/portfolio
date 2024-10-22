cfa010ee5127eab12e67b077ff168b651fcbe388
Jiří Hofman <jiri.hofman@gmail.com>
2024-10-22 21:11:50 +0200

    fix: update Link component to remove legacyBehavior for improved compatibility

--- app/projects/article.jsx
+++ app/projects/article.jsx
@@ -48,7 +48,7 @@ export const Article = async ({ project }) => {
                 </span>
             </div>
             
-            <Link href={appLink} legacyBehavior>
+            <Link href={appLink}>
                 <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display cursor-pointer" title={`Click to view the ${project.homepage ? 'app' : 'repo'}.`}>
                     <span className="bg-gradient-to-r from-purple-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-transparent bg-clip-text">
                         {project.name}
