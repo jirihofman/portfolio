d32937ba7dbfc54e31d295b261ee0b3ea75dde0b
Jiří Hofman <jiri.hofman@gmail.com>
2024-10-23 20:56:58 +0200

    client cache

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -1,13 +1,7 @@
 export const Card = ({ children }) => {
 
 	return (
-		<div
-			className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600"
-		>
-			<div className="pointer-events-none">
-				<div className="absolute inset-0 z-0 transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
-			</div>
-
+		<div className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-zinc-600">
 			{children}
 		</div>
 	);
--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-	experimental: {
-		// mdxRs: true,
-	},
-	env: {
-		/** GitHub username loaded in build time. */
-		GITHUB_USERNAME: await fetch('https://api.github.com/user',
-			{
-				headers: {
-					Authorization: `token ${process.env.GH_TOKEN}`,
-				},
-				next: {
-					// No revalidation needed. It is fine to get it on build time and use it forever.
-					tags: ['github', 'github-username'],
-				}
-			}
-		).then(res => res.json()).then(data => data.login),
-	},
-	images: {
-		remotePatterns: [
-			{ protocol: 'https', hostname: '**.githubusercontent.com' },
-			{ protocol: 'https', hostname: '**.github.com' }
-		],
-	},
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    experimental: {
+        // Caching all page.jsx files on the client for 5 minutes.
+        // Resulting in immediate navigation and no loading time.
+        staleTimes: {
+            dynamic: 300,
+            static: 300
+        }
+    },
+    env: {
+        /** GitHub username loaded in build time. */
+        GITHUB_USERNAME: await fetch('https://api.github.com/user',
+            {
+                headers: {
+                    Authorization: `token ${process.env.GH_TOKEN}`,
+                },
+                next: {
+                    // No revalidation needed. It is fine to get it on build time and use it forever.
+                    tags: ['github', 'github-username'],
+                }
+            }
+        ).then(res => res.json()).then(data => data.login),
+    },
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: '**.githubusercontent.com' },
+            { protocol: 'https', hostname: '**.github.com' }
+        ],
+    },
 };
 
 export default (nextConfig);
