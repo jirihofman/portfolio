52782d4a5b1abc2568bfdbc2b3c3da38a9e5c989
Jiří Hofman <jiri.hofman@gmail.com>
2024-10-23 13:30:51 +0200

    cache

--- app/data.js
+++ app/data.js
@@ -207,7 +207,7 @@ export const getTrafficPageViews = async (username, reponame) => {
     return { sumUniques, todayUniques };
 };
 
-export const getDependabotAlerts = async (username, reponame) => {
+export const getDependabotAlerts = unstable_cache(async (username, reponame) => {
     const res = await fetch('https://api.github.com/repos/' + username + '/' + reponame + '/dependabot/alerts', {
         headers: { Authorization: `Bearer ${process.env.GH_TOKEN}` },
         next: { revalidate: HOURS_12 }
@@ -227,7 +227,7 @@ export const getDependabotAlerts = async (username, reponame) => {
     }, {});
 
     return openAlertsBySeverity;
-};
+}, ['getDependabotAlerts'], { revalidate: HOURS_12 });
 
 /**
  * Determines if a repository is using Next.js App Router or legacy pages/_app.jsx. Or both.
